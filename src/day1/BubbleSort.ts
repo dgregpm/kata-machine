3328a97f6b6697fd02f8a4b59e6ac62de684c860
dgregpm <60014046+dgregpm@users.noreply.github.com>
2022-12-05 00:18:49 -0500

    Update BubbleSort.ts

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,14 @@
 export default function bubble_sort(arr: number[]): void {
+    
+    const swap = (a: number,b: number) => {
+        [arr[a],arr[b]] = [arr[b],arr[a]];
+    }
 
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr.length + 1 - i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(j,j+1);
+            }
+        }
+    }
 }

787fdd2cc501d64c658b8e842f9245a8fa8b9a24
dgregpm <60014046+dgregpm@users.noreply.github.com>
2022-12-06 23:31:53 -0500

    Update SinglyLinkedList.ts

--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,9 +1,17 @@
+type Node<T> = {
+    val: T | undefined;
+    next: Node<T> | undefined;
+}
+
 export default class SinglyLinkedList<T> {
     public length: number;
-
+    private head: Node<T> | undefined;
+    private tail: Node<T> | undefined;
     
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     prepend(item: T): void {
@@ -15,13 +23,22 @@ export default class SinglyLinkedList<T> {
     append(item: T): void {
 
 }
-    remove(item: T): T | undefined {
+//     remove(item: T): T | undefined {
 
-}
-    get(idx: number): T | undefined {
+// }
+//     get(idx: number): T | undefined {
 
-}
+// }
     removeAt(idx: number): T | undefined {
-
-}
+        let h = this.head;
+        let prev = h;
+        for(let i=1;i<this.length;i++){
+            prev = h;
+            h = h?.next;
+        }
+        if(h){
+            if(prev?.next){ prev.next = h?.next };
+            return h.val;
+        }
+    }
 }

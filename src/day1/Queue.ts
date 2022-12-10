49a8d92571516a0d83fa34db90b17982d9c3b2e9
dgregpm <60014046+dgregpm@users.noreply.github.com>
2022-12-09 23:42:28 -0500

    Update Queue.ts

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,43 @@
+class Node<T> {
+    public val: T;
+    public next: Node<T> | undefined;
+    constructor(item: T){
+        this.val = item;
+    }
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    public head: Node<T> | undefined;
+    public tail: Node<T> | undefined;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
-
     enqueue(item: T): void {
-
-}
+        if(!item){return;}
+        const node = new Node<T>(item);
+        if(this.length === 0) {
+            this.head = this.tail = node;
+            this.length++;
+        } else if(this.length > 0) {
+            if(this.tail){
+                this.tail.next = node;
+                this.tail = node;
+                this.length++;
+            }
+        }
+    }
     deque(): T | undefined {
-
-}
+        if(this.length === 0){return undefined;}
+        const value = this.head?.val;
+        this.head = this.head?.next;
+        this.length--;
+        return value;
+    }
     peek(): T | undefined {
-
-}
+        if(this.length === 0){return undefined;}
+        return this.head?.val
+    }
 }

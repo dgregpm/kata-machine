2c832aafed53d8ebc7c268496e8bace3791bb9f5
dgregpm <dgregpm@gmail.com>
2023-07-13 11:11:52 -0400

    Binary Search

--- src/day2/BinarySearchList.ts
+++ src/day2/BinarySearchList.ts
@@ -1,3 +1,7 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
     //Binary Search
+    let lo = 0;
+    let hi = haystack.length - 1;
+    let m = (hi + lo)/2;
+    
 }

c436aa5861b64066ad95cb92d280cfd6749065d8
dgregpm <dgregpm@gmail.com>
2023-07-13 18:12:16 -0400

    Update

--- src/day2/BinarySearchList.ts
+++ src/day2/BinarySearchList.ts
@@ -1,7 +1,30 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
     //Binary Search
     let lo = 0;
-    let hi = haystack.length - 1;
-    let m = (hi + lo)/2;
+    let hi = haystack.length;
+    let m = Math.floor((hi + lo)/2);
     
+    while(lo < hi){
+        console.log(`(lo:${lo}, hi:${hi}, mid:${m})\n`)
+        if(haystack[m] > needle){
+            hi = m;
+            m = Math.floor((hi + lo)/2);
+        } else if (haystack[m] < needle){
+            lo = m+1;
+            m = Math.floor((hi + lo)/2);
+        } else {
+            return true
+        }
+    }
+    return false;
 }
+
+function bs_list1(haystack: number[], needle: number): boolean{
+    let lo = 0;
+    let hi = haystack.length;
+    let m = Math.floor((hi - lo)/2);
+    while(lo<hi)
+
+const foo = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
+
+console.log(bs_list(foo, 2));

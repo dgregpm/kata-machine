0400882ba68276403b255fd5296b056fc4cf8602
dgregpm <dgregpm@gmail.com>
2023-08-22 18:05:08 -0400

    Update

--- src/day4/BinarySearchList.ts
+++ src/day4/BinarySearchList.ts
@@ -1,4 +1,4 @@
-export default function bs_list(haystack: number[], needle: number): boolean {
+export function bs_list1(haystack: number[], needle: number): boolean {
     let lo = 0;
     let hi = haystack.length;
     let m = Math.floor((hi+lo)/2);
@@ -12,6 +12,25 @@ export default function bs_list(haystack: number[], needle: number): boolean {
             hi = m;
         }
         m = Math.floor((hi+lo)/2);
+    }
+    return false;
+}
+
+
+
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let high = haystack.length;
+
+    while(lo<high){
+        let mid = Math.floor((high+lo)/2);
+        if(needle === haystack[mid]){
+            return true;
+        } else if(needle > haystack[mid]){
+            lo = mid + 1;
+        } else if(needle < haystack[mid]){
+            high = mid;
+        }        
     }
     return false;
 }
--- src/day4/BubbleSort.ts
+++ src/day4/BubbleSort.ts
@@ -1,3 +1,11 @@
 export default function bubble_sort(arr: number[]): void {
-
+    let n = arr.length;
+    while(n>0){
+        for(let j=0;j<n-1;j++){
+            if(arr[j] > arr[j+1]){
+                [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
+            }
+        }
+        n--;
+    }
 }

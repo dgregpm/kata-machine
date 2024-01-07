d305848dd15685e0dbfa83c45cd88139e57d386e
dgregpm <dgregpm@gmail.com>
2024-01-06 23:34:26 -0500

    New Year

--- src/day6/BinarySearchList.ts
+++ src/day6/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+	let lo = 0;
+	let hi = haystack.length;
+	
+	while(lo < hi){
+		let mid = Math.floor((lo + hi)/2);
+		if(haystack[mid] === needle){
+			return true;
+		}else if(haystack[mid] > needle){
+			hi = mid;
+		}else if(haystack[mid] < needle){
+			lo = mid + 1;
+		}
 
+	}
+	return false;
 }

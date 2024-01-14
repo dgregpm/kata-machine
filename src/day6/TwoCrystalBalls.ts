8967884f69d8cd27847efc6946fbc70874a30ead
EC2 Default User <ec2-user@ip-172-31-89-210.ec2.internal>
2024-01-14 02:01:29 +0000

    Complete solution

--- src/day6/TwoCrystalBalls.ts
+++ src/day6/TwoCrystalBalls.ts
@@ -1,3 +1,17 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    let sqrt = Math.sqrt(breaks.length);
+    let i = 0;
+    for ( ; i < breaks.length ; i+=sqrt) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    i -= sqrt;
+    for(let j=0;j<sqrt && i<breaks.length;j++,i++){
+        if(breaks[i]){
+            return i;
+        }
+    }
+    return -1;
 }
+    

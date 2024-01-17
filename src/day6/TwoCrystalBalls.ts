886d0be7d85da1e2d59f730f9e7b116388bb88f3
dgregpm <dgregpm@gmail.com>
2024-01-17 07:16:45 -0500

    Shrug

--- src/day6/TwoCrystalBalls.ts
+++ src/day6/TwoCrystalBalls.ts
@@ -1,4 +1,5 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+<<<<<<< HEAD
     let sqrt = Math.sqrt(breaks.length);
     let i = 0;
     for ( ; i < breaks.length ; i+=sqrt) {
@@ -15,3 +16,20 @@ export default function two_crystal_balls(breaks: boolean[]): number {
     return -1;
 }
     
+=======
+	let sqrt = Math.sqrt(breaks.length);
+	let i = sqrt;
+	for( ; i<breaks.length ; i+=sqrt){
+		if(breaks[i]){
+			break;
+		}	
+	}
+	i-=sqrt;
+	for(let j = i; j<breaks.length && j< i+sqrt ;j++){
+		if(breaks[j]){
+			return j;
+		}
+	}
+	return -1;
+}
+>>>>>>> 1b2a5b0 (Shrug)
